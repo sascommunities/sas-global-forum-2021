75c42328d7d670aed2308e20b65ad21cac167f90
Jared <jared.dean@gmail.com>
2021-05-07 13:20:06 -0400

    test on trial env

---
+++ binder/sascfg_personal.py
@@ -0,0 +1,8 @@
+SAS_config_names = ['viya4']
+
+viya4 = {
+    'url':'https://pdcesx13124.my-trials.sas.com',
+    'verify': False,
+    'timeout': 30,
+    'context':'SAS Job Execution compute context'
+    }
